3e101c7308851c2396a9761d00865dec3e1b3c0b
Mrizkyy <79984517+rizkyyuhari@users.noreply.github.com>
2023-01-31 16:12:06 +0700

    Add files via upload

---
+++ dbConnection.js
@@ -0,0 +1,19 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+const pool = new Pool({
+  host: "localhost",
+  database: "maktabahDb",
+  password: "260999",
+  user: "postgres",
+});
+
+const { Sequelize } = require("sequelize");
+const db = new Sequelize("maktabahDb", "postgres", "260999", {
+  host: "localhost",
+  dialect: "postgres",
+});
+
+module.exports = {
+  query: (text, param) => pool.query(text, param),
+  db,
+};
